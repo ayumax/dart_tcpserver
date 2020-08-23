0438f1f224600848038f885e287f274ee42059e3
ayumax <ayuma0913@gmail.com>
2020-08-23 22:11:53 +0900

    init

---
+++ bin/dart_tcpserver.dart
@@ -0,0 +1,5 @@
+import 'package:dart_tcpserver/dart_tcpserver.dart' as dart_tcpserver;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_tcpserver.calculate()}!');
+}
---
+++ test/dart_tcpserver_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_tcpserver/dart_tcpserver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
